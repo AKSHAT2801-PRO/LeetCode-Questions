7aa1e1f26d5111ac726bc21b6c94ab2c0ca11c85
AKSHAT2801-PRO <75081417+AKSHAT2801-PRO@users.noreply.github.com>
2026-03-05 13:16:29 +0530

    [1025] [Time Beats: 100.00%] [Memory Beats: 99.50%] - LeetPush

---
+++ Divisor-Game.cpp
@@ -0,0 +1,12 @@
+1class Solution {
+2public:
+3    bool divisorGame(int n) {
+4        if (n%2 == 0){
+5            return true;
+6        }
+7        else{
+8            return false;
+9        }
+10    }
+11       
+12};
