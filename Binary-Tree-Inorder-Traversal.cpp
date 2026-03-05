ba2cb887fc37fdc69234dc66fc084389e12397be
AKSHAT2801-PRO <75081417+AKSHAT2801-PRO@users.noreply.github.com>
2026-03-05 13:10:33 +0530

    [94] [Time Beats: 100.00%] [Memory Beats: 34.44%] - LeetPush

---
+++ Binary-Tree-Inorder-Traversal.cpp
@@ -0,0 +1,28 @@
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    void method(TreeNode* root,vector<int> &ans){
+15        if (!root){
+16            return;
+17        }
+18        method(root->left,ans);
+19        ans.push_back(root->val);
+20        method(root->right,ans);
+21        return;
+22    }
+23    vector<int> inorderTraversal(TreeNode* root){
+24        vector<int> ans;
+25        method(root,ans);
+26        return ans;
+27    }
+28};
