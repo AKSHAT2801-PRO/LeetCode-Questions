42eb42cf08c5d08dc947f9aa23a6071cd7810c2f
AKSHAT2801-PRO <75081417+AKSHAT2801-PRO@users.noreply.github.com>
2026-03-05 13:10:19 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 42.66%] - LeetPush

---
+++ Binary-Tree-Preorder-Traversal.cpp
@@ -0,0 +1,30 @@
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    void method(TreeNode* root,vector<int> &ans){
+15        if(!root){
+16            return;
+17        }
+18        // root->left->right
+19        ans.push_back(root->val); //root
+20        method(root->left,ans); //left
+21        method(root->right,ans); //right
+22        return;
+23
+24    }
+25    vector<int> preorderTraversal(TreeNode* root) {
+26        vector<int> ans;
+27        method(root,ans);
+28        return ans;
+29    }
+30};
